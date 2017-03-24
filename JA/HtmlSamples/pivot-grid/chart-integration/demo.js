ba2cb4eddb0b2f4a2cbf920f4dd7f13dc011650a
Flaming Watermelons <flamingwatermelons@example.com>
2017-03-24 06:44:57 -0400

    SB build 16.2.20162.93

--- JA/HtmlSamples/pivot-grid/chart-integration/demo.js
+++ JA/HtmlSamples/pivot-grid/chart-integration/demo.js
@@ -154,7 +154,7 @@ $(function () {
                         horizontalZoomable: true,
                         verticalZoomable: true,
                         windowResponse: "immediate",
-                        title: "Revenue per Units Sold"
+                        title: "販売単位ごとの売上"
                     });
                 };
 
