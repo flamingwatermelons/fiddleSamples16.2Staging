4247854bc75670fe2786d8f914cada4f1bb13e82
Flaming Watermelons <flamingwatermelons@example.com>
2017-03-30 04:08:09 -0400

    SB build 16.2.20162.103

--- JA/HtmlSamples/combo/cascading-combos/demo.js
+++ JA/HtmlSamples/combo/cascading-combos/demo.js
@@ -78,7 +78,7 @@ var dsCountry, dsCascTowns, dsCountryCascading,
             $("#comboCountry").igCombo({
                 textKey: "txtCountry",
                 valueKey: "valCountry",
-                placeHolder: "select country...",
+                placeHolder: "国を選択",
                 dataSource: dsCountry,
                 selectionChanged: function (evt, ui) {
                     var filteredCascDistrict = [];
@@ -114,7 +114,7 @@ var dsCountry, dsCascTowns, dsCountryCascading,
             $("#comboDistrict").igCombo({
                 valueKey: "valDistrict",
                 textKey: "txtDistrict",
-                placeHolder: "select district...",
+                placeHolder: "区を選択",
                 dataSource: [],
                 disabled: true,
                 selectionChanged: function (evt, ui) {
@@ -141,7 +141,7 @@ var dsCountry, dsCascTowns, dsCountryCascading,
             $("#comboTown").igCombo({
                 valueKey: "valTown",
                 textKey: "textTown",
-                placeHolder: "select town...",
+                placeHolder: "市を選択",
                 disabled: true
             });
         });
